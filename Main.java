400784127154b3dffd16702dec2811262b62146a
andrija <astojanovic321@gmail.com>
2020-12-30 13:30:22 +0100

    Auto stash before merge of "main" and "origin/main"

--- Main.java
+++ Main.java
@@ -7,8 +7,11 @@ public class Main{
 	}
 
 	public static void andrija(){
+		System.out.println("Andrija");
+		System.out.println("123");
 		System.out.println("Izmena metode");
 	}
+
 
 	public static void uros(){
 		System.out.println("Uros");
